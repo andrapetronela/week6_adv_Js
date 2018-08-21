b1c5294bc164a9e41f12389869be941962adab68
Andra <andra.popa1989@gmail.com>
2018-08-21 17:40:05 +0100

    updating content in html

---
+++ script.js
@@ -0,0 +1,10 @@
+// create a button to click above and update #anavirginia to be selections
+
+// $(function() {
+//   var selections = []
+//   $("#myButton").click(function() {
+//     selections.push(prompt("name"))
+//       $("#anavirginia").text(selections)
+//   })
+//    
+// });
