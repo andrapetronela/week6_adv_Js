079b791ecde6c3e016f4224062f0a6bfb7677809
Andra <andra.popa1989@gmail.com>
2018-08-24 20:31:19 +0100

    try, catch block

--- exercisePromises.js
+++ exercisePromises.js
@@ -104,3 +104,27 @@ const getData = async function() {
   console.log('posta', posts);
   console.log('albums', albums);
 }
+
+// #3) Add a try catch block to the #2 solution in order to catch any errors.
+// Now chnage one of the urls so you console.log your error with 'ooooooops'
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholderypicode.com/albums'
+]
+
+const getData = async function() {
+    try {
+  const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+      const resp = await fetch(url)
+            return respo.json()      
+     } 
+  ));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
+    catch(err) {
+    console.log("Ooooooooops!", err);
+}
+}
