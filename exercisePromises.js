0c9d37f4aa9ca25f53adee74b29f003e8e246db5
Andra <andra.popa1989@gmail.com>
2018-08-24 17:09:52 +0100

    async await added

--- exercisePromises.js
+++ exercisePromises.js
@@ -68,3 +68,39 @@ Promise.all(urls.map(url =>
     console.log('4', array[3])
   })
   .catch(err => console.log('Ops! 404 is here again!', err));
+
+
+// Solve the below problems:
+
+// #1) Convert the below promise into async await
+fetch('https://swapi.co/api/starships/9/')
+  .then(response => response.json())
+  .then(console.log)
+
+async function getUrl() {
+    const response = await fetch('https://swapi.co/api/starships/9/')
+    const data = await response.json()
+    console.log(data)
+}
+
+}
+// #2) ADVANCED: Update the function below to also have
+// async await for this line: fetch(url).then(resp => resp.json())
+// So there shouldn't be any .then() calls anymore!
+// Don't get discouraged... this is a really tough one...
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
+
+const getData = async function() {
+  const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+      const resp = await fetch(url)
+            return respo.json()      
+     } 
+  ));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
+}
